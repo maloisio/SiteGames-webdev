57c3cf6be35b0d2c0241ddf532a8be58e84bc5cb
Mauro <maloisio_otto@hotmail.com>
2021-11-15 21:14:48 -0300

    mudancas produtosJS

---
+++ js/script-produtos.js
@@ -0,0 +1,35 @@
+var listaprodutos = [];
+
+function adicionarprod(clicked_id) {
+
+    if(clicked_id == 'gamecyberpunk'){
+
+        listaprodutos.push(clicked_id);
+    }
+    if (clicked_id == 'gamebattlefield2042') {
+
+        listaprodutos.push(clicked_id);
+    }
+    if (clicked_id == 'gamedmc') {
+
+        listaprodutos.push(clicked_id);
+    }
+    if (clicked_id == 'gamegodofwar') {
+
+        listaprodutos.push(clicked_id);
+    }
+    if (clicked_id == 'gamenewworld') {
+
+        listaprodutos.push(clicked_id);
+    }
+    if (clicked_id == 'gamefifa22') {
+
+        listaprodutos.push(clicked_id);
+    }
+    if (clicked_id == 'gameforza5') {
+
+        listaprodutos.push(clicked_id);
+    }
+ 
+        window.localStorage.setItem("produtos", JSON.stringify(listaprodutos));
+}
